0f27b2095f0722bcf42c4c834d9d8214108f46a2
Gusmang Adi <cybermoon3@gmail.com>
2025-01-02 18:32:20 +0800

    perbaiki keranjang error

--- resources/views/keranjang-index.blade.php
+++ resources/views/keranjang-index.blade.php
@@ -8,85 +8,92 @@
         <div class="kontainer-keranjang">
             <div class="keranjang-item">
                 <div class="keranjang-per-item">
-                    @foreach($keranjangs as $item)
-                        <div class="keranjang-detail-toko">
-                            {{-- Informasi toko --}}
-                            <div class="keranjang-header">
-                                <div class="keranjang-nama-toko">
-                                    <span>
-                                        {{ $item->produks->penjual->nama_toko }}
-                                    </span>
+                    @if ($keranjangs->isNotEmpty())
+                        @foreach($keranjangs as $item)
+                            <div class="keranjang-detail-toko">
+                                {{-- Informasi toko --}}
+                                <div class="keranjang-header">
+                                    <div class="keranjang-nama-toko">
+                                        <span>
+                                            {{ $item->produks->penjual->nama_toko }}
+                                        </span>
+                                    </div>
+                                </div>
+
+                                {{-- Detail produk --}}
+                                <div class="keranjang-detail">
+                                    <img class="keranjang-gambar-produk" src="{{ asset('images/' . $item->produks->gambar_produk) }}" alt="{{ $item->produks->nama_produk }}" />
+
+                                    <div class="keranjang-detail-produk">
+                                        {{-- Nama produk --}}
+                                        <p class="keranjang-nama-produk">
+                                            {{ $item->produks->nama_produk }}
+                                        </p>
+
+                                        @if ($item->produks->diskon->isNotEmpty())
+
+                                            {{-- {{dd($item->produks->harga)}} --}}
+                                            {{-- {{dd()}} --}}
+                                            {{-- {{dd($item->produks->harga * $item->produks->diskon->first()->persentase_diskon / 100)}} --}}
+                                            {{-- Harga produk --}}
+                                            <p class="keranjang-harga-produk">
+                                                Total: Rp<span
+                                                    id="total-{{ $item->id_keranjang }}">{{ number_format(($item->produks->harga - ($item->produks->harga * $item->produks->diskon->first()->persentase_diskon / 100)) * $item->jumlah, 0, ',', '.') }}
+                                                </span>
+                                            </p>
+
+                                        @else
+
+                                            {{-- Harga produk --}}
+                                            <p class="keranjang-harga-produk">
+                                                Total: Rp<span
+                                                    id="total-{{ $item->id_keranjang }}">{{ number_format($item->produks->harga * $item->jumlah, 0, ',', '.') }}
+                                                </span>
+                                            </p>
+
+                                        @endif
+
+                                        {{-- <p class="keranjang-harga-produk">
+                                            Rp{{ number_format($item->produks->harga, 0, ',', '.') }}
+                                        </p> --}}
+
+                                    </div>
+
+                                    <div class="jumlah" style="padding-top: 7%; width: fit-content;">
+                                        <!-- Tombol untuk mengurangi jumlah -->
+                                        <button class="minus"
+                                            style="width: 10%; font-weight: bold; font-size: 18px; background-color: #046ad6; color: white; border-radius: 4px; padding: 6px 6px; cursor: pointer"
+                                            data-id="{{ $item->id_keranjang }}" data-action="minus"
+                                            data-id-produk="{{ $item->id_produk }}">-</button>
+                                        <input type="number" value="{{ $item->jumlah }}" min="1" id="jumlah-{{ $item->id_keranjang }}"
+                                            class="quantity"
+                                            style="width: 25%; height: 12%; text-align: center; font-weight: 400; font-size: 16px; background-color: #e7e7e7; color: rgb(39, 39, 39); border-radius: 4px;">
+                                        <!-- Tombol untuk menambah jumlah -->
+                                        <button class="plus"
+                                            style="width: 10%; aspect-ratio: 1/1; font-weight: bold; font-size: 18px; background-color: #046ad6; color: white; border-radius: 4px; padding: 6px 6px; cursor: pointer;"
+                                            data-id="{{ $item->id_keranjang }}" data-action="plus"
+                                            data-id-produk="{{ $item->id_produk }}">+</button>
+                                    </div>
+
+                                    {{-- Hapus produk --}}
+                                    <div class="keranjang-aksi-produk">
+                                        <form action="{{ route('keranjangs.destroy', $item->id_keranjang) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="keranjang-hapus-barang" type="submit">Hapus</button>
+                                        </form>
+                                    </div>
+
                                 </div>
                             </div>
 
-                            {{-- Detail produk --}}
-                            <div class="keranjang-detail">
-                                <img class="keranjang-gambar-produk" src="{{ asset('images/' . $item->produks->gambar_produk) }}" alt="{{ $item->produks->nama_produk }}" />
+                        @endforeach
 
-                                <div class="keranjang-detail-produk">
-                                    {{-- Nama produk --}}
-                                    <p class="keranjang-nama-produk">
-                                        {{ $item->produks->nama_produk }}
-                                    </p>
+                    @else
 
-                                    @if ($item->produks->diskon->isNotEmpty())
+                    @endif
 
-                                        {{-- {{dd($item->produks->harga)}} --}}
-                                        {{-- {{dd()}} --}}
-                                        {{-- {{dd($item->produks->harga * $item->produks->diskon->first()->persentase_diskon / 100)}} --}}
-                                        {{-- Harga produk --}}
-                                        <p class="keranjang-harga-produk">
-                                            Total: Rp<span
-                                                id="total-{{ $item->id_keranjang }}">{{ number_format(($item->produks->harga - ($item->produks->harga * $item->produks->diskon->first()->persentase_diskon / 100)) * $item->jumlah, 0, ',', '.') }}
-                                            </span>
-                                        </p>
 
-                                    @else
-
-                                        {{-- Harga produk --}}
-                                        <p class="keranjang-harga-produk">
-                                            Total: Rp<span
-                                                id="total-{{ $item->id_keranjang }}">{{ number_format($item->produks->harga * $item->jumlah, 0, ',', '.') }}
-                                            </span>
-                                        </p>
-
-                                    @endif
-
-                                    {{-- <p class="keranjang-harga-produk">
-                                        Rp{{ number_format($item->produks->harga, 0, ',', '.') }}
-                                    </p> --}}
-
-                                </div>
-
-                                <div class="jumlah" style="padding-top: 7%; width: fit-content;">
-                                    <!-- Tombol untuk mengurangi jumlah -->
-                                    <button class="minus"
-                                        style="width: 10%; font-weight: bold; font-size: 18px; background-color: #046ad6; color: white; border-radius: 4px; padding: 6px 6px; cursor: pointer"
-                                        data-id="{{ $item->id_keranjang }}" data-action="minus"
-                                        data-id-produk="{{ $item->id_produk }}">-</button>
-                                    <input type="number" value="{{ $item->jumlah }}" min="1" id="jumlah-{{ $item->id_keranjang }}"
-                                        class="quantity"
-                                        style="width: 25%; height: 12%; text-align: center; font-weight: 400; font-size: 16px; background-color: #e7e7e7; color: rgb(39, 39, 39); border-radius: 4px;">
-                                    <!-- Tombol untuk menambah jumlah -->
-                                    <button class="plus"
-                                        style="width: 10%; aspect-ratio: 1/1; font-weight: bold; font-size: 18px; background-color: #046ad6; color: white; border-radius: 4px; padding: 6px 6px; cursor: pointer;"
-                                        data-id="{{ $item->id_keranjang }}" data-action="plus"
-                                        data-id-produk="{{ $item->id_produk }}">+</button>
-                                </div>
-
-                                {{-- Hapus produk --}}
-                                <div class="keranjang-aksi-produk">
-                                    <form action="{{ route('keranjangs.destroy', $item->id_keranjang) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class="keranjang-hapus-barang" type="submit">Hapus</button>
-                                    </form>
-                                </div>
-
-                            </div>
-                        </div>
-
-                    @endforeach
                 </div>
 
                 <div class="keranjang-ringkasan-belanja">
@@ -94,28 +101,29 @@
                         Total Harga Belanja
                     </h3>
 
-                    @if ($item->produks->diskon->isNotEmpty())
-
-                        {{-- Jumlah Harga --}}
-                        <p id="total-all" style="font-size: 20px; font-weight: 400; margin-bottom: 4%;">
-                            Rp{{number_format($keranjangs->sum(function ($item) {
-                                $diskon = $item->produks->diskon->first();
-                                $harga_produk = $item->produks->harga - ($item->produks->harga * $diskon->persentase_diskon / 100);
-                                return $harga_produk * $item->jumlah; }), 0, ',', '.')
-                            }}
-                        </p>
-
-                    @else
-
-                        <p id="total-all" style="font-size: 20px; font-weight: 400; margin-bottom: 4%;">
-                            Rp{{number_format($keranjangs->sum(function ($item) {
-                                return $item->produks->harga * $item->jumlah; }), 0, ',', '.')
-                            }}
-                        </p>
-
-                    @endif
-
                     @if ($keranjangs->isNotEmpty())
+
+                        @if ($item->produks->diskon->isNotEmpty())
+
+                            {{-- Jumlah Harga --}}
+                            <p id="total-all" style="font-size: 20px; font-weight: 400; margin-bottom: 4%;">
+                                Rp{{number_format($keranjangs->sum(function ($item) {
+                                    $diskon = $item->produks->diskon->first();
+                                    $harga_produk = $item->produks->harga - ($item->produks->harga * $diskon->persentase_diskon / 100);
+                                    return $harga_produk * $item->jumlah; }), 0, ',', '.')
+                                }}
+                            </p>
+
+                        @else
+
+                            <p id="total-all" style="font-size: 20px; font-weight: 400; margin-bottom: 4%;">
+                                Rp{{number_format($keranjangs->sum(function ($item) {
+                                    return $item->produks->harga * $item->jumlah; }), 0, ',', '.')
+                                }}
+                            </p>
+
+                        @endif
+
                         <a href="{{ route('pesanan.create') }}" class="btn-beli bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
                             Beli
                         </a>
